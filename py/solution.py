d66279945c35a605a9635537bc1f580805a635d3
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-12-18 14:35:46 -0800

    move angle calculation functions inside of satellite class

--- py/solution.py
+++ py/solution.py
@@ -3,8 +3,54 @@ from typing import Dict, List, Tuple
 from util import Color, Sat, User, Vector3
 import math
 
-MAX_USERS_PER_SAT = 32
 COLORS = [Color.A, Color.B, Color.C, Color.D]
+
+def solve(users: Dict[User, Vector3], sats: Dict[Sat, Vector3]) -> Dict[User, Tuple[Sat, Color]]:
+    solution = {}
+
+    for satellite_id in sats:
+        Satellite.satellites[satellite_id] = Satellite(satellite_id, sats, users)
+
+    for user_id in users:
+        User.users[user_id] = User(user_id, users)
+
+    for satellite_id in Satellite.satellites.keys():
+        for user_id in Satellite.satellites[satellite_id].viable_users:
+            for color in COLORS:
+                if Satellite.satellites[satellite_id].available(user=user_id, color=color):
+                    solution[user_id] = (satellite_id, color)
+                    User.users[user_id].assigned = True
+                    Satellite.satellites[satellite_id].assign(user_id, color)
+                    break  
+
+    for satellite_id, satellite in Satellite.satellites.items():
+        for user_id in satellite.viable_users:
+            for color in COLORS:
+                if Satellite.satellites[satellite_id].available(user=user_id, color=color):
+                    solution[user_id] = (satellite_id, color)
+                    User.users[user_id].assigned = True
+                    Satellite.satellites[satellite_id].assign(user_id, color)
+                    break
+
+    for satellite_id, satellite in Satellite.satellites.items():
+        for user_id in satellite.viable_users:
+            for color in COLORS:
+                if satellite.can_make_room_for(user_id, color):
+                    reassignments = satellite.make_room_for(user_id, color)
+                    for user_to_reassign, color_to_reassign_to in reassignments.items():
+                        if Satellite.satellites[satellite_id].available():
+                            solution[user_to_reassign] = (satellite_id, color_to_reassign_to)
+                            Satellite.satellites[satellite_id].unassign(user_to_reassign)
+                            Satellite.satellites[satellite_id].assign(user_to_reassign, color_to_reassign_to)
+
+                    if Satellite.satellites[satellite_id].available():
+                        solution[user_id] = (satellite_id, color)
+                        User.users[user_id].assigned = True
+                        Satellite.satellites[satellite_id].assign(user_id, color)
+                      
+    return solution
+
+MAX_USERS_PER_SAT = 32
 
 class Satellite:
     satellites = {}
@@ -17,12 +63,12 @@ class Satellite:
 
     @property
     def viable_users(self):
-        return [user_id for user_id in self.users if not User.users[user_id].assigned and is_beam_within_45_degrees(self.users[user_id], self.vector)]
+        return [user_id for user_id in self.users if not User.users[user_id].assigned and self.is_beam_within_45_degrees(self.users[user_id], self.vector)]
 
     def available(self, **kwargs):
         if 'user' in kwargs and 'color' in kwargs:
             return len(self.assignments) < MAX_USERS_PER_SAT and all(
-            not is_user_within_10_degrees(self.vector, self.users[kwargs['user']], self.users[assignment['user']])
+            not self.is_user_within_10_degrees(self.vector, self.users[kwargs['user']], self.users[assignment['user']])
             for assignment in self.assignments if assignment['color'] == kwargs['color']
         )
 
@@ -31,7 +77,7 @@ class Satellite:
     def conflicts(self, user, color):
         return [
             assignment['user'] for assignment in self.assignments
-            if assignment['color'] == color and is_user_within_10_degrees(self.vector, self.users[user], self.users[assignment['user']])
+            if assignment['color'] == color and self.is_user_within_10_degrees(self.vector, self.users[user], self.users[assignment['user']])
         ]
 
     def assign(self, user, color):
@@ -77,70 +123,21 @@ class Satellite:
                 break  # Stop checking conflicts if one cannot be reassigned
         return reassignments
 
+    def is_beam_within_45_degrees(self, user, satellite):
+        return 180 - user.angle_between(Vector3(0, 0, 0), satellite) <= 45
+
+    def is_user_within_10_degrees(self, satellite, user_1, user_2):
+        vec1 = user_1 - satellite 
+        vec2 = user_2 - satellite
+
+        if vec1.mag() == 0 or vec2.mag() == 0:
+            return False
+
+        return satellite.angle_between(user_1, user_2) < 10
+
 class User:
     users = {}
 
     def __init__(self, id, users):
         self.assigned = False
-
-def solve(users: Dict[User, Vector3], sats: Dict[Sat, Vector3]) -> Dict[User, Tuple[Sat, Color]]:
-    solution = {}
-
-    for satellite_id in sats:
-        Satellite.satellites[satellite_id] = Satellite(satellite_id, sats, users)
-
-    for user_id in users:
-        User.users[user_id] = User(user_id, users)
-
-    for satellite_id in Satellite.satellites.keys():
-        for user_id in Satellite.satellites[satellite_id].viable_users:
-            for color in COLORS:
-                if Satellite.satellites[satellite_id].available(user=user_id, color=color):
-                    solution[user_id] = (satellite_id, color)
-                    User.users[user_id].assigned = True
-                    Satellite.satellites[satellite_id].assign(user_id, color)
-                    break  
-
-    for satellite_id, satellite in Satellite.satellites.items():
-        for user_id in satellite.viable_users:
-            for color in COLORS:
-                if Satellite.satellites[satellite_id].available(user=user_id, color=color):
-                    solution[user_id] = (satellite_id, color)
-                    User.users[user_id].assigned = True
-                    Satellite.satellites[satellite_id].assign(user_id, color)
-                    break
-
-    for satellite_id, satellite in Satellite.satellites.items():
-        for user_id in satellite.viable_users:
-            for color in COLORS:
-                satellite = Satellite.satellites[satellite_id]
-                if satellite.can_make_room_for(user_id, color):
-                    reassignments = satellite.make_room_for(user_id, color)
-                    for user_to_reassign, color_to_reassign_to in reassignments.items():
-                        if Satellite.satellites[satellite_id].available():
-                            solution[user_to_reassign] = (satellite_id, color_to_reassign_to)
-                            Satellite.satellites[satellite_id].unassign(user_to_reassign)
-                            Satellite.satellites[satellite_id].assign(user_to_reassign, color_to_reassign_to)
-
-                    if Satellite.satellites[satellite_id].available():
-                        solution[user_id] = (satellite_id, color)
-                        User.users[user_id].assigned = True
-                        Satellite.satellites[satellite_id].assign(user_id, color)
-                      
-    return solution
-
-def is_beam_within_45_degrees(user, satellite):
-    return 180 - user.angle_between(Vector3(0, 0, 0), satellite) <= 45
-
-def is_user_within_10_degrees(satellite, user_1, user_2):
-    vec1 = user_1 - satellite 
-    vec2 = user_2 - satellite 
-    
-    magnitude1 = vec1.mag()
-    magnitude2 = vec2.mag()
-
-    if magnitude1 == 0 or magnitude2 == 0:
-        return False
-
-    return satellite.angle_between(user_1, user_2) < 10
 
